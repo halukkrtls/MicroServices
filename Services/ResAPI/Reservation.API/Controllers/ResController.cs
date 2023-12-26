f4a2a7ef7a747be82ffebeb9f597d9d3bda104ad
halukkrtls <haluk_kurtulus20@erdogan.edu.tr>
2023-12-26 12:38:14 +0300

    Proje dosyası ekle.

---
+++ Services/ResAPI/Reservation.API/Controllers/ResController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Reservation.API.Infrastructure;
+using Reservation.API.Services;
+
+namespace Reservation.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ResController : ControllerBase
+    {
+        private readonly IReservationService reservationService;
+
+        public ResController(IReservationService ReservationService)
+        {
+            reservationService = ReservationService;
+        }
+
+        [HttpGet("{Id}")]
+        public Models.ReservationDTO Get(int Id)
+        {
+            return reservationService.GetResByBkgNumber(Id);
+        }
+    }
+}
